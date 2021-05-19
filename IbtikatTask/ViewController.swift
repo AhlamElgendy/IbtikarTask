45449b31c52f9dc82f8a7dcf1ac00761ffb57f7e
Ahlam Ismail <ahlam.ismail1992@gmail.com>
2021-05-19 20:31:04 +0200

    Initial Commit

---
+++ IbtikatTask/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  IbtikatTask
+//
+//  Created by Ahlam on 19/5/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
